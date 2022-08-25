caca6c4082ec98b408e0f50c04c3e6f0802eea53
Eyþór Óli Borgþórsson <58181979+eythorb19@users.noreply.github.com>
2022-08-25 09:50:16 +0000

    Add files via upload

---
+++ BST_solutions/bst_map_tests.py
@@ -0,0 +1,107 @@
+from bst_map import *
+
+def test_map(m):
+    try:
+        m.insert(5, "fimma")
+    except ItemExistsException:
+        print("Item already exists")
+    try:
+        m.insert(4, "fjarri")
+    except ItemExistsException:
+        print("Item already exists")
+    try:
+        m.insert(2, "tvistur")
+    except ItemExistsException:
+        print("Item already exists")
+    try:
+        m.insert(5, "fimmarimma")
+    except ItemExistsException:
+        print("Item already exists")
+    m[1] = "ás"
+
+    try:
+        m.update(4, "fjarkalarki")
+    except NotFoundException:
+        print("Item not found")
+    try:
+        m.update(6, "sexxxxxa")
+    except NotFoundException:
+        print("Item not found")
+
+    m[6] = "sexa"
+
+    print("size of map: " + str(len(m)))
+    print(m.contains(12))
+    m[12] = "drottning"
+    print(m.contains(12))
+
+    print("size of map: " + str(len(m)))
+    try:
+        print(m.find(4))
+    except NotFoundException:
+        print("Item not found")
+    try:
+        print(m[2])
+    except NotFoundException:
+        print("Item not found")
+    try:
+        print(m[1])
+    except NotFoundException:
+        print("Item not found")
+    try:
+        print(m[5])
+    except NotFoundException:
+        print("Item not found")
+    try:
+        print(m.find(6))
+    except NotFoundException:
+        print("Item not found")
+    try:
+        print(m[7])
+    except NotFoundException:
+        print("Item not found")
+
+    print("size of map: " + str(len(m)))
+    try:
+        m.remove(5)
+        print("Item removed")
+    except NotFoundException:
+        print("Item not found")
+    try:
+        print(m.find(5))
+    except NotFoundException:
+        print("Item not found")
+    try:
+        m.remove(5)
+        print("Item removed")
+    except NotFoundException:
+        print("Item not found")
+        
+    print("size of map: " + str(len(m)))
+
+    print(m)
+    
+    try:
+        m.insert(12, "drolla")
+    except ItemExistsException:
+        print("Item already exists")
+    print(m)
+
+    try:
+        m.insert(6, "sixxer")
+    except ItemExistsException:
+        print("Item already exists")
+    print(m)
+
+    try:
+        m.insert(9, "nía")
+    except ItemExistsException:
+        print("Item already exists")
+    print(m)
+
+
+if __name__ == "__main__":
+    print("\nTESTING BSTMAP")
+    m = BSTMap()
+    test_map(m)
+    
